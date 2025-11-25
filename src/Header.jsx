194978bd83aedf947dca789d6e10169b848d2d5c
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-11-25 15:56:29 +0100

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import style from "./Header.module.css"
+import logImg from "./assets/img/dc-logo.png"
+export default function Header () {
+   return( 
+    <header>
+        <img src="{LogImg}" alt="" />
+    </header>
+   )
+
+}
