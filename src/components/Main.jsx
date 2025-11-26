d513b42e48e90d87746524aa0eb0f447105dcd08
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-11-26 17:32:11 +0100

    Feat: manca da dinamizzare la nav card complete

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,36 +4,53 @@ import MerchandiseImg from "./Merchandise"
 import Subscriptions from "./Suscriptio"
 import Locator from "./Locator"
 import VisaPower from "./PowerVisa"
+import Comics from "../assets/comics"
 
 export default function Main() {
     return (
         <main>
-            <div className="bg-dark">
-                <div className="container text-white d-flex aling-items-center h">
-                    <p> <strong>--&#62; Content goes Here &#60;--</strong> </p>
+            
+            <div className={style.banner}>
+                <h3>Current Series</h3>
+            </div>
+            <div className="bg-dark list-comics">
+                <div className="container text-white d-flex aling-items-center wrap gap jc-between">
+                    {Comics.map((curComics) => {
+                        return (
+                            <div className="col " key = {curComics.id}>
+                                <img className={style.comicimg} src={curComics.thumb} alt="" />
+                                <h4 className="titolo">{curComics.series}</h4>
+                                <p className={style.prezzo}>{curComics.price}</p>
+
+                            </div>
+                        )
+
+                    })}
+                 
+
                 </div>
             </div>
             <div className="bg-blue h ">
                 <ul className="d-flex container jc-between aling-items-center h">
                     <li className="d-flex aling-items-center gap-10 text-white">
-                        <Comic/>
+                        <Comic />
                         <p>DIGITAL COMINCS</p>
 
                     </li>
                     <li className="d-flex aling-items-center gap-10 text-white">
-                        <MerchandiseImg/>
+                        <MerchandiseImg />
                         <p>DIGITAL COMINCS</p>
 
                     </li><li className="d-flex aling-items-center gap-10 text-white">
-                        <Subscriptions/>
+                        <Subscriptions />
                         <p>DIGITAL COMINCS</p>
 
                     </li><li className="d-flex aling-items-center gap-10 text-white">
-                        <Locator/>
+                        <Locator />
                         <p>DIGITAL COMINCS</p>
 
                     </li><li className="d-flex aling-items-center gap-10 text-white">
-                        <VisaPower/>
+                        <VisaPower />
                         <p>DIGITAL COMINCS</p>
 
                     </li>
